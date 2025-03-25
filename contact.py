5f877c56eb4597decd674d1dd61222ff687364ee
M.Lokadithya <132217025+Lokad1thya@users.noreply.github.com>
2025-03-25 17:56:17 +0530

    Update contact.py

--- contact.py
+++ contact.py
@@ -3,4 +3,4 @@ import streamlit as st
 def contact():
     st.title("Contact Page")
     st.write("You can contact us here.")
-    st.subheader("@kruthi @pallavi @lokadithya @aryan @Someone I dont remember")
+    st.subheader("@sathwik @manohar @lokadithya @Someone I dont remember")
